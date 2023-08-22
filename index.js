df9e7dc11314a58a03c3911a2caee1d503a07919
tonni-akter-dev <tanniakhter123@gmail.com>
2023-08-22 12:33:08 +0600

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 const { MongoClient } = require("mongodb");
-const uri = `mongodb+srv://shop-mart:PORPhpsCezyW6Uye@cluster0.qtpo1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtpo1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
